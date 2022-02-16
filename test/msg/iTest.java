345b7391741761e2600449c4d5f212a1678bd9f7
gemsvido <kid4cars@gmail.com>
2022-02-16 14:59:25 -0800

    MessageEngine
    - I'm improving tests, getting ready for 0.2.0

--- test/msg/iTest.java
+++ test/msg/iTest.java
@@ -18,6 +18,7 @@ final class iTest {
         i.line("Test");
         System.out.println(i.white + "\n]\n\n\n");    }
 
+
     /** This tests the <strong>line</strong> method in the <strong>i</strong> class, with an <strong>Object</strong> passed in VarArgs <p></p>
      * @author gemsvidø
      * @since 0.2.0  */
@@ -27,6 +28,7 @@ final class iTest {
         i.line(obj);
         System.out.println(i.white + "\n]\n\n\n");    }
 
+
     /** This tests the <strong>line</strong> method in the <strong>i</strong> class, with nothing passed in VarArgs, printing an empty line. <p></p>
      * @author gemsvidø
      * @since 0.2.0  */
@@ -35,11 +37,49 @@ final class iTest {
         i.line();
         System.out.println(i.white + "\n]\n\n\n");    }
 
+
     /** This tests the <strong>line</strong> method in the <strong>i</strong> class, with an <strong>integer</strong> passed in VarArgs <p></p>
      * @author gemsvidø
      * @since 0.2.0  */
     @Test void testLine4 () {
         System.out.println("\niTest.testLine4  [\n");
+        i.line(69);
+        System.out.println(i.white + "\n]\n\n\n");    }
+
+
+    /** This tests the <strong>text</strong> method in the <strong>i</strong> class, with a <strong>String</strong> passed in VarArgs <p></p>
+     * @author gemsvidø
+     * @since 0.2.0  */
+    @Test void testText1 () {
+        System.out.println("\niTest.testText1  [\n");
+        i.text("Test");
+        System.out.println(i.white + "\n]\n\n\n");    }
+
+
+    /** This tests the <strong>text</strong> method in the <strong>i</strong> class, with an <strong>Object</strong> passed in VarArgs <p></p>
+     * @author gemsvidø
+     * @since 0.2.0  */
+    @Test void testText2 () {
+        System.out.println("\niTest.testText2  [\n");
+        Object obj = new Object();
+        i.text(obj);
+        System.out.println(i.white + "\n]\n\n\n");    }
+
+
+    /** This tests the <strong>text</strong> method in the <strong>i</strong> class, with nothing passed in VarArgs, printing literally nothing. <p></p>
+     * @author gemsvidø
+     * @since 0.2.0  */
+    @Test void testText3 () {
+        System.out.println("\niTest.testText3  [\n");
+        i.text();
+        System.out.println(i.white + "\n]\n\n\n");    }
+
+
+    /** This tests the <strong>text</strong> method in the <strong>i</strong> class, with an <strong>integer</strong> passed in VarArgs <p></p>
+     * @author gemsvidø
+     * @since 0.2.0  */
+    @Test void testText4 () {
+        System.out.println("\niTest.testText4  [\n");
         i.line(69);
         System.out.println(i.white + "\n]\n\n\n");    }
 
@@ -55,14 +95,13 @@ final class iTest {
         i.text("Print method: " + i.cyan);
         i.cb.Print();
 
-        i.text("Line method: " + i.cyan);
+        i.text("\nLine method: " + i.cyan);
         i.cb.PrintLine();
 
         i.text("GetContents method: "  + i.cyan + i.cb.GetContents());
 
         i.line(i.white + "\n]\n\n\n");
     }
-
 
 
     /** This tests the <strong>debugLine</strong> method in the <strong>i</strong> class, with debug mode <strong>on</strong>.  <p></p>
@@ -83,6 +122,7 @@ final class iTest {
         i.debugLine("Test", "Debug line works!");
         System.out.println(i.white + "\n]\n\n\n");    }
 
+
     /** This tests the <strong>display</strong> method in the <strong>i</strong> class. <p></p>
      * @author gemsvidø
      * @since 0.2.0  */
@@ -93,6 +133,7 @@ final class iTest {
         i.display(msg);
         System.out.println(i.white + "\n]\n\n\n");    }
 
+
     /** This tests the <strong>message</strong> method in the <strong>i</strong> class. <p></p>
      * @author gemsvidø
      * @since 0.2.0  */
@@ -102,6 +143,7 @@ final class iTest {
         TextMessage msg = new TextMessage(afi, "Message Text for message()", i.localServer);
         i.display(msg);
         System.out.println(i.white + "\n]\n\n\n");    }
+
 
     /** This tests the <strong>ProcessInput</strong> method in the <strong>i</strong> class. <p></p>
      * @author gemsvidø
@@ -117,14 +159,27 @@ final class iTest {
         i.ProcessInput("/uhfpiahhawihawif"); // Unknown command
         System.out.println(i.white + "\n]\n\n\n");
     }
-    
+
+
+    /** This tests the <strong>getToken</strong> method in the <strong>i</strong> class. <p></p>
+     * @author gemsvidø
+     * @since 0.2.0  */
     @Test void getToken () {
-        System.out.println("\niTest.getToken  [\n");
-
-
-
-        System.out.println(i.white + "\n]\n\n\n");
+        i.line("\niTest.getToken  [\n");
+        i.line("Token: " + i.cyan + i.getToken());
+        i.line(i.white + "\n]\n\n\n");
     }
+
+
+    /** This tests the <strong>info</strong> method in the <strong>i</strong> class. <p></p>
+     * @author gemsvidø
+     * @since 0.2.0  */
+    @Test void info () {
+        i.line("\niTest.info  [\n");
+        i.info("Text");
+        i.line(i.white + "\n]\n\n\n");
+    }
+
 
     /** Private constructor prevents utility class <strong>iTest</strong> from being initialized. <p></p>
      * @author gemsvidø */
